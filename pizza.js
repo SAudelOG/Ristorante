937aa3f0a4b520e0508a421ec888ac3cca50766b
SAudelOG <audel91@gmail.com>
2015-06-21 19:47:45 -0500

    handlebars engine set and refactr

--- pizza.js
+++ pizza.js
@@ -5,6 +5,20 @@
 
 	var app = express();
 
+	var handlebars = require('express-handlebars').create({
+		defaultLayout: 'main',
+		helpers:{
+			section: function(name, options){
+				if(!this._sections) this._sections = {};
+				this._sections[name] = options.fn(this);
+				return null;
+			}
+		}
+	});
+
+	app.engine('handlebars', handlebars.engine);
+	app.set('view engine', 'handlebars');
+
 	app.set('port', process.env.PORT || 3000);
 
 	app.use(express.static(__dirname + '/public'));
