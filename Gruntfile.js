0e8b903d04117d4f008672691437d00866556c42
SAudelOG <audel91@gmail.com>
2015-06-21 22:02:49 -0500

    Gruntready to test and jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,16 @@
 module.exports = function (grunt) {
 
+  //Load Plugin
+  [
+    'grunt-contrib-watch',
+    'grunt-contrib-jshint',
+    'grunt-cafe-mocha'
+  ]
+  .forEach(function(task){
+    grunt.loadNpmTasks(task);
+  });
+
+  //Configure plugins
   grunt.initConfig({
     watch: {
       project: {
@@ -8,11 +19,23 @@ module.exports = function (grunt) {
           livereload: true
         }
       }
+    },
+    cafemocha: {
+      all:{
+        src:'qa/tests-*.js',
+        options:{
+          ui:'tdd'
+        },
+      }
+    },
+    jshint:{
+      app:['pizza.js', './Public/js/main.js'],
+      qa:['Gruntfile.js', './Public/qa/**/*.js', './qa/**/*.js']
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  //Resgister Tasks
+  grunt.registerTask('default', ['cafemocha', 'jshint']);
 
-  grunt.registerTask('default', ['watch']);
-
+  runt.registerTask('watch', ['watch']);
 };
